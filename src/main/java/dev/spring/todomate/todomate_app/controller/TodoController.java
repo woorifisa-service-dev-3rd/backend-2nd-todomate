5d12ba6fc9357631598b14ecc8a919f1802b01f2
seonmin5 <seonmin5555@gmail.com>
2024-09-06 20:10:34 +0900

    feat: update 기능 추가

--- src/main/java/dev/spring/todomate/todomate_app/controller/TodoController.java
+++ src/main/java/dev/spring/todomate/todomate_app/controller/TodoController.java
@@ -26,32 +26,6 @@ public class TodoController {
     @Autowired
     private final UserService userService;
 
-    @GetMapping("/addTodo")
-    public String processAddForm(Model model) {
-        model.addAttribute("todo", Todo.builder().build());
-        return "createOrUpdateTodoForm";
-    }
-
-    @PostMapping("/add")
-    public String addTodo(@ModelAttribute Todo todo) {
-        System.out.println("todo = " + todo);
-        todo.setStartDate(LocalDate.now());
-        todo.setDueDate(LocalDate.now());
-
-        // 로그인 세션에서 User id를 가져와서 Repository에서 해당하는 id를 가져와서 해야하는데, 우선은 test용으로 1을 넣고 해 보자
-        // User service에서 findbyid 가져오기
-        // 여기에 setter로 해당 id 넣어주기
-        // 테스트용 User ID: 1
-        Long userId = 1L;
-        User user = userService.findById(userId);
-        todo.setUser(user);
-        todoService.addTodoDetails(todo);
-
-        System.out.println("user = " + user);
-        System.out.println("todo = " + todo);
-
-        return "redirect:/todos/list";
-    }
 
     @GetMapping("/list")
     public String showTodoList(Model model) {
@@ -72,4 +46,41 @@ public class TodoController {
         return "createOrUpdateTodoForm"; // 수정 또는 생성 폼 JSP 파일 이름
     }
 
+    @GetMapping("/addTodo")
+    public String processAddForm(Model model) {
+        model.addAttribute("todo", Todo.builder().build());
+        return "createOrUpdateTodoForm";
+    }
+
+    @PostMapping("/add")
+    public String addTodo(@ModelAttribute Todo todo) {
+        todo.setStartDate(LocalDate.now());
+        todo.setDueDate(LocalDate.now());
+
+        // 로그인 세션에서 User id를 가져와서 Repository에서 해당하는 id를 가져와서 해야하는데, 우선은 test용으로 1을 넣고 해 보자
+        // User service에서 findbyid 가져오기
+        // 여기에 setter로 해당 id 넣어주기
+        // 테스트용 User ID: 1
+        Long userId = 1L;
+        User user = userService.findById(userId);
+        todo.setUser(user);
+        todoService.addTodoDetails(todo);
+
+        return "redirect:/todos/list";
+    }
+
+    @PostMapping("/update")
+    public String updateTodo(@ModelAttribute Todo todo) {
+        todo.setStartDate(LocalDate.now());
+        todo.setDueDate(LocalDate.now());
+
+        // 테스트용 ID: 1
+        Long userId = 1L;
+        User user = userService.findById(userId);
+        todo.setUser(user);
+        todoService.addTodoDetails(todo);
+
+        return "redirect:/todos/list";
+
+    }
 }
