00562bcbd60ad137243553ea49204b0feaa3feeb
yaejinkong <0910a@naver.com>
2024-09-10 17:02:28 +0900

    refactor: @Repository 애노테이션 추가

--- src/main/java/dev/spring/todomate/todomate_app/repository/DiaryRepository.java
+++ src/main/java/dev/spring/todomate/todomate_app/repository/DiaryRepository.java
@@ -1,12 +1,13 @@
 package dev.spring.todomate.todomate_app.repository;
 
 import dev.spring.todomate.todomate_app.model.Diary;
-import dev.spring.todomate.todomate_app.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findAllByUserId(Long userId);
     Optional<Diary> findDiaryByUserIdAndId(Long userId, Long id);
